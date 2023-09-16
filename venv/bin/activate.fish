0afb30db166667c1c01516bf7d09d7b198a1a480
adil_ma <itsadilhadid@gmail.com>
2023-09-16 22:38:51 +0100

    updated pip

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/adil/PROJECTS/personal/todo-app-flask/venv"
+set -gx VIRTUAL_ENV "/home/adil/PROJECTS/personal/Todo-App-Flask-Sql_ORM/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
