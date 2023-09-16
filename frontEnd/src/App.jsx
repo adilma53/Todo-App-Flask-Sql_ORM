71e72f6747cd1dea6f315040f4e8f0f6209aa606
adil_ma <itsadilhadid@gmail.com>
2023-09-16 23:39:21 +0100

    updated

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -5,6 +5,7 @@ function App()
 {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState('');
+  const apiUrl = 'https://your-render-backend-url'; // Replace with your actual Render backend URL
 
   useEffect(() =>
   {
@@ -15,7 +16,7 @@ function App()
   {
     try
     {
-      const response = await axios.get('http://localhost:5000/api/todos');
+      const response = await axios.get(`${apiUrl}/api/todos`);
       console.log('res ->', response);
       setTodos(response.data);
     } catch (error)
@@ -29,7 +30,7 @@ function App()
     if (newTodo.trim() === '') return;
     try
     {
-      const response = await axios.post('http://localhost:5000/api/todos', { text: newTodo });
+      const response = await axios.post(`${apiUrl}/api/todos`, { text: newTodo });
       console.log('res ->', response);
       setNewTodo('');
       fetchTodos();
@@ -43,7 +44,7 @@ function App()
   {
     try
     {
-      await axios.put(`http://localhost:5000/api/todos/${id}`, {
+      await axios.put(`${apiUrl}/api/todos/${id}`, {
         text,
         completed,
       });
@@ -58,7 +59,7 @@ function App()
   {
     try
     {
-      await axios.delete(`http://localhost:5000/api/todos/${id}`);
+      await axios.delete(`${apiUrl}/api/todos/${id}`);
       fetchTodos();
     } catch (error)
     {
@@ -67,18 +68,18 @@ function App()
   };
 
   return (
-    <div className="bg-gradient-to-b from-slate-900 via-slate-800 to-slate-600  text-white min-h-screen py-8 font-roboto">
+    <div className="bg-gradient-to-b from-slate-900 via-slate-800 to-slate-600 text-white min-h-screen py-8 font-roboto">
       <div className="max-w-md mx-auto p-4 border border-slate-500 rounded-lg shadow-lg">
         <h1 className="text-3xl font-extrabold mb-6 text-center">Todo List</h1>
 
         <div className="flex mb-7">
           <input
-            className=" flex-grow px-4 py-2 bg-white text-black rounded-l focus:outline-none text-base font-semibold"
+            className="flex-grow px-4 py-2 bg-white text-black rounded-l focus:outline-none text-base font-semibold"
             type="text"
             placeholder="Add a new todo"
             value={newTodo}
             onChange={(e) => setNewTodo(e.target.value)}
-            onKeyPress={e => (e.key === 'Enter') ? addTodo() : ''}
+            onKeyPress={(e) => (e.key === 'Enter' ? addTodo() : '')}
           />
           <button
             className="border-b border-t border-r border-slate-500 px-4 py-2 bg-slate-900 text-red-600 rounded-r hover:bg-red-600 hover:text-white transition duration-300 ease-in-out text-base font-semibold"
@@ -90,7 +91,6 @@ function App()
 
         <ul>
           {todos.map((todo) => (
-
             <li
               key={todo.id}
               className="rounded-md border border-slate-500 flex items-center mb-4 bg-slate-900 rounded-lg px-4 py-2 transition duration-300 ease-in-out hover:bg-slate-700"
@@ -119,7 +119,7 @@ function App()
           ))}
         </ul>
       </div>
-    </div >
+    </div>
   );
 }
 
